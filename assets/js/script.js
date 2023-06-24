7a9684cecdf535a92b08f55c6c597e54e8271dbe
Michael-Tio <mtio161@gmail.com>
2023-06-25 00:29:21 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,13 @@ function scrollToSection(sectionId) {
 function openURL(url) {
     window.open(url, '_blank');
 }
+
+function toggleMenu() {
+    var menu = document.querySelector('.main-menu'); // Mengambil elemen menu navigasi
+    
+    if (menu.style.display === 'flex') {
+      menu.style.display = 'none'; // Menyembunyikan menu jika sedang ditampilkan
+    } else {
+      menu.style.display = 'flex'; // Menampilkan menu jika sedang disembunyikan
+    }
+  }
