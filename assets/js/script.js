8f91fd3f4b978c1c7ba0d32a2f4025be8fe79794
Michael-Tio <mtio161@gmail.com>
2023-06-25 17:31:47 +0700

    animations and fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,65 @@ function toggleMenu() {
     } else {
       menu.style.display = 'flex'; // Menampilkan menu jika sedang disembunyikan
     }
-  }
+}
+
+// Fade In Scroll Animation
+const observerFade = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showFade');
+    }
+    else{
+      entry.target.classList.remove('showFade');
+    }
+  });
+});
+
+// Go Up Scroll Animation
+const observerUp = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showGoUp');
+    }
+    else{
+      entry.target.classList.remove('showGoUp');
+    }
+  });
+});
+
+// Go Left Scroll Animation
+const observerLeft = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showFromLeft');
+    }
+    else{
+      entry.target.classList.remove('showFromLeft');
+    }
+  });
+});
+
+// Go Right Scroll Animation
+const observerRight = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showFromRight');
+    }
+    else{
+      entry.target.classList.remove('showFromRight');
+    }
+  });
+});
+
+const hiddenElementsFade = document.querySelectorAll('.hiddenFade');
+hiddenElementsFade.forEach((el) => observerFade.observe(el));
+const hiddenElementsUp = document.querySelectorAll('.hiddenGoUp');
+hiddenElementsUp.forEach((el) => observerUp.observe(el));
+const hiddenElementsLeft = document.querySelectorAll('.hiddenToLeft');
+hiddenElementsLeft.forEach((el) => observerLeft.observe(el));
+const hiddenElementsRight = document.querySelectorAll('.hiddenToRight');
+hiddenElementsRight.forEach((el) => observerRight.observe(el));
